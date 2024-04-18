8795c3c2d89cfbfa7faff1b641f15f9b75fb33be
Luis Gutierrez <81604875+lcgc99@users.noreply.github.com>
2024-04-18 16:52:44 -0500

    try except blocks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Response
 from fastapi.responses import RedirectResponse
 import pandas as pd
 import sqlite3
@@ -9,41 +9,52 @@ COLUMN_NAMES = {
     'departments': ['Id', 'Department'],
     'jobs': ['Id', 'Job'],
 }
-BATCH_SIZE = 1000
+BATCH_LIMIT = 1000
 
 app = FastAPI()
+
 
 @app.get("/TestEndpoint")
 async def root():
     return {"message": "starting the API development"}
 
+
 @app.get("/")
 async def read_root():
     return RedirectResponse("/docs")
 
+
 @app.get("/Data/{file_id}")
 async def get_data(file_id: str):
+    try:
+        # Use Pandas to read the CSV data
+        df = pd.read_csv(f'{file_id}.csv', header=None, dtype="string")
 
-    # Use Pandas to read the CSV data
-    df = pd.read_csv(f'{file_id}.csv', header=None, dtype="string")
+        # Convert DataFrame to a list of dictionaries
+        data = df.to_dict(orient='records')
 
-    # Convert DataFrame to a list of dictionaries
-    data = df.to_dict(orient='records')
+        return {"data": data}
 
-    return {"data": data}
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
 
 @app.post("/Upload/{file_id}")
 async def upload_data(file_id: str, skip: int = 0):
+    try:
+        df = pd.read_csv(f'{file_id}.csv', header=None, dtype="string")
+        df.columns = COLUMN_NAMES[file_id]
 
-    df = pd.read_csv(f'{file_id}.csv', header=None, dtype="string")
-    df.columns = COLUMN_NAMES[file_id]
+        with sqlite3.connect(DATABASE_NAME) as conn:
+            df.iloc[skip: skip + BATCH_LIMIT].to_sql(
+                # SQL doesn't consider case but Python does
+                name=str.upper(file_id),
+                con=conn,
+                if_exists='append',
+                index=False,
+            )
 
-    with sqlite3.connect(DATABASE_NAME) as conn:
-        df.iloc[skip:].to_sql(
-            # SQL doesn't consider case but Python does
-            name=str.upper(file_id),
-            con=conn,
-            if_exists='append',
-            index=False,
-            chunksize=BATCH_SIZE
-        )
+        return Response(status_code=204)
+
+    except Exception as e:
+        raise HTTPException(status_code=409, detail=str(e))
